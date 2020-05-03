bb2d751aef64e143397b90244394b5a7758ae7c6
George Spake <gpspake@gmail.com>
2020-05-03 18:32:51 -0500

    add swagger

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 
 namespace TodoApi
 {
@@ -37,6 +38,11 @@ namespace TodoApi
                 opt.UseInMemoryDatabase("TodoList"));
 
             services.AddControllers();
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +60,14 @@ namespace TodoApi
             app.UseAuthentication();
 
             app.UseAuthorization();
+            
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseEndpoints(endpoints =>
             {
